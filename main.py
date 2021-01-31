17050db511b27f9cecc5b6abd7ee57304ce82515
ehsan <bakefayat98@gmail.com>
2021-01-31 17:51:34 +0330

    start work with design classes

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import mysql.connector
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    passwd = ""
-)
-cursor = mydb.cursor()
+class drug:
+    def __init__(self, id, name, conflicts):
+        self.id = id
+        self.name = name
+        self.conflicts = conflicts
+
+
+class patient:
+    def __init__(self, name, family, gender, address, history):
+        self.name = name
+        self.family = family
+        self.gender = gender
+        self.address = address
+        self.history = history
+#mydb = mysql.connector.connect(
+#    host = "localhost",
+#   user = "root",
+#    passwd = ""
+#)
+#cursor = mydb.cursor()
+p1 = patient('ehsan','bakefayat','male', 'iran', 'heart diesase')
+print(p1.name)
